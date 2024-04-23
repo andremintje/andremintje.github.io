e732011f974c597119cb69da9fe088ae0e3b8124
Andre Mintje <andremintje026@student.unsrat.ac.id>
2024-04-23 22:00:26 +0800

    Update Jscript.js

--- JS/Jscript.js
+++ JS/Jscript.js
@@ -2,17 +2,17 @@ function showLoader() {
     document.getElementById("loader").style.display = "block";
   }
 
-  // Function to hide the loading animation
+
   function hideLoader() {
     document.getElementById("loader").style.display = "none";
   }
 
-  // Event listener to hide the loading animation when the new page is loaded
+
   window.addEventListener('beforeunload', function () {
     showLoader();
   });
 
-  // Event listener to hide the loading animation when the new page is loaded
+
   window.addEventListener('load', function () {
     hideLoader();
   });
